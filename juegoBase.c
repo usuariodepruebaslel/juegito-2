6bf752e78cc70ece9f45eea739f80a53487f20c0
Jesus Medina <59062804+TheYisusPls@users.noreply.github.com>
2021-08-30 13:41:46 -0400

    este es el juego base

---
+++ juegoBase.c
@@ -0,0 +1,30 @@
+int main(int argc, int a, char** argv)
+{
+	printf("Una minita se te acerca y te dice\n\n");
+	printf("-uwu?\n\n");
+	printf("La minita es re linda y re anime\n\n");
+	printf("Que haces?\n\n");
+	printf("1.Decirle uwu de vuelta\n2.Decirle que la chupe\n3.Ignorarla epicamente\n\n");
+	printf("Elige una opcion: ");
+	scanf("%d",&a);
+	if (a==1)
+	{ printf("\nLa minita se pone contenta y se hacen amigos, pasa el tiempo, pololean, se casan,\ntienen hijos y al final mueren juntos ya viejitos\n\nGood Ending\n"); }
+	else if (a==2)
+	{ printf("\nLa minita se enoja y te funa en Instagram, Facebook, Twitter, Whattsapp, Youtube, Reddit, 4chan y Twitch\n");
+	  printf("Un grupo de simps de la minita te lincha, eres internado de gravedad en la clinica Davila y mueres\nproducto de las heridas y la mala atencion del sistema de salud publica\n");
+	  printf("Y encima como la minita mintio diciendo que supuestamente la violaste, un grupo de desconocidos desentierran\ntu cuerpo y lo tiran al mar, ya ni descanzar en paz puedes\n\nBadEnding\n\n");
+	}
+	else if (a==69)
+	{printf("\n\nNice\n\n");}
+	else if (a==3)
+	{ printf("\n*la ignora epicamente* Te vas sin decir nada, jaja que aweonao perdiste la oportunidad\n\nNeutral Ending\n\n");}
+	else if (a==11)
+	{ printf("\n\nChupala entonce\n\n");}
+	else if (a==4)
+	{ printf("\n\nComo tu vieja\n\n");}
+	else if (a==5)
+	{ printf("\n\nPor el culo te la inco\n\n");}
+	else
+	{ printf("\n\nIngresa una opcion valida po aweonao\n\n");}
+	return 0;
+}
